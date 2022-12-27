2fe6563bd7f1c70e39fc3b215f80e5aa4553ec92
yuritakagi <takagiyuri0712@gmail.com>
2022-12-27 18:49:47 +0900

    fix: rubocopに従って修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,50 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe User do
   it "名前、メール、パスワード、パスワード(確認)があれば有効であること" do
-    user = FactoryBot.build(:user)
+    user = build(:user)
     expect(user).to be_valid
   end
 
   it "名前がなければ無効であること" do
-    user = FactoryBot.build(:user, name: nil)
+    user = build(:user, name: nil)
     user.valid?
     expect(user.errors[:name]).to include("can't be blank")
   end
 
   it "名前は20字以内であること" do
-    user = FactoryBot.build(:user, name: 'a' * 21)
+    user = build(:user, name: 'a' * 21)
     user.valid?
     expect(user.errors[:name]).to include("is too long (maximum is 20 characters)")
   end
 
   it "メールがなければ無効であること" do
-    user = FactoryBot.build(:user, email: nil)
+    user = build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
 
   it "重複したメールなら無効であること" do
-    FactoryBot.create(:user)
-    user = FactoryBot.build(:user)
+    create(:user)
+    user = build(:user)
     user.valid?
     expect(user.errors[:email]).to include("has already been taken")
   end
 
   it "パスワードがなければ無効であること" do
-    user = FactoryBot.build(:user, password: nil)
+    user = build(:user, password: nil)
     user.valid?
     expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
   end
 
   it "パスワードは最低6字であること" do
-    user = FactoryBot.build(:user, password: 'pw')
+    user = build(:user, password: 'pw')
     user.valid?
     expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
   end
 
   it "パスワード(確認)がなければ無効であること" do
-    user = FactoryBot.build(:user, password_confirmation: nil)
+    user = build(:user, password_confirmation: nil)
     user.valid?
     expect(user.errors[:password_confirmation]).to include("can't be blank")
   end
