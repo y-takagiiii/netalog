66de501747ffe0af9b34841f75b24457af21ffbe
yuritakagi <takagiyuri0712@gmail.com>
2022-12-27 19:15:17 +0900

    add: 日本語化の設定を追加

--- config/application.rb
+++ config/application.rb
@@ -28,8 +28,11 @@ module Netatube
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Tokyo"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/.{rb, yml}').to_s]
 
     # Don't generate system test files.
     config.generators.system_tests = nil
