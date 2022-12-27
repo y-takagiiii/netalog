bb8cfabcac78ff40f9cf9ba85b0008e56767cbad
yuritakagi <takagiyuri0712@gmail.com>
2022-12-27 21:15:38 +0900

    fix: rubocopに従って修正

--- config/application.rb
+++ config/application.rb
@@ -30,10 +30,10 @@ module Netatube
     #
     config.time_zone = "Tokyo"
     # config.eager_load_paths << Rails.root.join("extras")
-    
+
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/.{rb, yml}').to_s]
-    
+
     config.generators do |g|
       g.skip_routes true
       g.assets false
@@ -42,10 +42,9 @@ module Netatube
         view_specs: false,
         helper_specs: false,
         routing_specs: false
-    end  
-  
+    end
+
     # Don't generate system test files.
     config.generators.system_tests = nil
-
   end
 end
