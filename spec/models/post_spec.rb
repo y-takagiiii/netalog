c048408fa0385913d6ee51004c2bc5b33ff6babc
yuritakagi <takagiyuri0712@gmail.com>
2022-12-28 20:30:46 +0900

    add: annotateを使ってスキーマ情報を追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,22 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
+RSpec.describe Post do
   pending "add some examples to (or delete) #{__FILE__}"
 end
