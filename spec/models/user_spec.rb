9287539c00200640f90e8f24c54ba4060bb89fd2
yuritakagi <takagiyuri0712@gmail.com>
2022-12-28 10:43:51 +0900

    fix: モデルスペックを日本語化用に修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,43 +9,43 @@ RSpec.describe User do
   it "名前がなければ無効であること" do
     user = build(:user, name: nil)
     user.valid?
-    expect(user.errors[:name]).to include("can't be blank")
+    expect(user.errors[:name]).to include("を入力してください")
   end
 
-  it "名前は20字以内であること" do
+  it "名前は20文字以内であること" do
     user = build(:user, name: 'a' * 21)
     user.valid?
-    expect(user.errors[:name]).to include("is too long (maximum is 20 characters)")
+    expect(user.errors[:name]).to include("は20文字以内で入力してください")
   end
 
   it "メールがなければ無効であること" do
     user = build(:user, email: nil)
     user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
+    expect(user.errors[:email]).to include("を入力してください")
   end
 
   it "重複したメールなら無効であること" do
     create(:user)
     user = build(:user)
     user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
+    expect(user.errors[:email]).to include("はすでに存在します")
   end
 
   it "パスワードがなければ無効であること" do
     user = build(:user, password: nil)
     user.valid?
-    expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+    expect(user.errors[:password]).to include("は6文字以上で入力してください")
   end
 
   it "パスワードは最低6字であること" do
     user = build(:user, password: 'pw')
     user.valid?
-    expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+    expect(user.errors[:password]).to include("は6文字以上で入力してください")
   end
 
   it "パスワード(確認)がなければ無効であること" do
     user = build(:user, password_confirmation: nil)
     user.valid?
-    expect(user.errors[:password_confirmation]).to include("can't be blank")
+    expect(user.errors[:password_confirmation]).to include("を入力してください")
   end
 end
