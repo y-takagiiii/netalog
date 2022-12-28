2589646aa764642ec0f36aad90c2d666ae3d5891
yuritakagi <takagiyuri0712@gmail.com>
2022-12-28 20:26:58 +0900

    create: postモデルを作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
 class Post < ApplicationRecord
   belongs_to :user
 end
