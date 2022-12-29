68cd749e1564848f312353d967618aba9daaf3d0
yuritakagi <takagiyuri0712@gmail.com>
2022-12-29 10:58:23 +0900

    refactor: user、videoのモデルスペックをリファクタリング

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,50 +17,50 @@
 require 'rails_helper'
 
 RSpec.describe User do
+  let(:user) { build(:user) }
+
   it "名前、メール、パスワード、パスワード(確認)があれば有効であること" do
-    user = build(:user)
     expect(user).to be_valid
   end
 
   it "名前がなければ無効であること" do
-    user = build(:user, name: nil)
+    user.name = nil
     user.valid?
     expect(user.errors[:name]).to include("を入力してください")
   end
 
   it "名前は20文字以内であること" do
-    user = build(:user, name: 'a' * 21)
+    user.name = 'a' * 21
     user.valid?
     expect(user.errors[:name]).to include("は20文字以内で入力してください")
   end
 
   it "メールがなければ無効であること" do
-    user = build(:user, email: nil)
+    user.email = nil
     user.valid?
     expect(user.errors[:email]).to include("を入力してください")
   end
 
   it "重複したメールなら無効であること" do
     create(:user)
-    user = build(:user)
     user.valid?
     expect(user.errors[:email]).to include("はすでに存在します")
   end
 
   it "パスワードがなければ無効であること" do
-    user = build(:user, password: nil)
+    user.password = nil
     user.valid?
     expect(user.errors[:password]).to include("は6文字以上で入力してください")
   end
 
   it "パスワードは最低6字であること" do
-    user = build(:user, password: 'pw')
+    user.password = 'pw'
     user.valid?
     expect(user.errors[:password]).to include("は6文字以上で入力してください")
   end
 
   it "パスワード(確認)がなければ無効であること" do
-    user = build(:user, password_confirmation: nil)
+    user.password_confirmation = nil
     user.valid?
     expect(user.errors[:password_confirmation]).to include("を入力してください")
   end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -14,37 +14,38 @@
 require 'rails_helper'
 
 RSpec.describe Video do
+  let(:video) { build(:video) }
+
   it 'youtube_id、詳細(概要欄)、サムネイル、タイトル、再生回数があれば有効であること' do
-    video = build(:video)
     expect(video).to be_valid
   end
 
   it "youtube_idがなければ無効であること" do
-    video = build(:video, youtube_id: nil)
+    video.youtube_id = nil
     video.valid?
     expect(video.errors[:youtube_id]).to include("を入力してください")
   end
 
   it "詳細(概要欄)がなければ無効であること" do
-    video = build(:video, description: nil)
+    video.description = nil
     video.valid?
     expect(video.errors[:description]).to include("を入力してください")
   end
 
   it "サムネイルがなければ無効であること" do
-    video = build(:video, thumbnail: nil)
+    video.thumbnail = nil
     video.valid?
     expect(video.errors[:thumbnail]).to include("を入力してください")
   end
 
   it "タイトルがなければ無効であること" do
-    video = build(:video, title: nil)
+    video.title = nil
     video.valid?
     expect(video.errors[:title]).to include("を入力してください")
   end
 
   it "再生回数がなければ無効であること" do
-    video = build(:video, view_count: nil)
+    video.view_count = nil
     video.valid?
     expect(video.errors[:view_count]).to include("を入力してください")
   end
