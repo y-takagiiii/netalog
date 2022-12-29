aa7320df2cd4a9d003f3eda2455a70f3692b888f
yuritakagi <takagiyuri0712@gmail.com>
2022-12-29 10:16:43 +0900

    fix: rubocopに従って修正

--- db/migrate/20221229000232_add_video_ref_to_post.rb
+++ db/migrate/20221229000232_add_video_ref_to_post.rb
@@ -1,5 +1,5 @@
 class AddVideoRefToPost < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :video, null: false, foreign_key: true
+    add_reference :posts, :video, null: false, default: 0, foreign_key: true
   end
 end
