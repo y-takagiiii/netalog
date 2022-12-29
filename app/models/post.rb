f26530ee2ac85b8a53a4b4699cf68f142df04781
yuritakagi <takagiyuri0712@gmail.com>
2022-12-29 09:30:39 +0900

    add: 関連付けを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,15 +6,19 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  user_id    :integer          not null
+#  video_id   :integer          not null
 #
 # Indexes
 #
-#  index_posts_on_user_id  (user_id)
+#  index_posts_on_user_id   (user_id)
+#  index_posts_on_video_id  (video_id)
 #
 # Foreign Keys
 #
-#  user_id  (user_id => users.id)
+#  user_id   (user_id => users.id)
+#  video_id  (video_id => videos.id)
 #
 class Post < ApplicationRecord
   belongs_to :user
+  belongs_to :video
 end
