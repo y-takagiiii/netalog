c8956e2c53616a66999315d961cda2ab247cbda2
yuritakagi <takagiyuri0712@gmail.com>
2022-12-29 10:32:17 +0900

    add: postのモデルスペックを追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,19 +6,22 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  user_id    :integer          not null
+#  video_id   :integer          default(0), not null
 #
 # Indexes
 #
-#  index_posts_on_user_id  (user_id)
+#  index_posts_on_user_id   (user_id)
+#  index_posts_on_video_id  (video_id)
 #
 # Foreign Keys
 #
-#  user_id  (user_id => users.id)
+#  user_id   (user_id => users.id)
+#  video_id  (video_id => videos.id)
 #
 require 'rails_helper'
 
 RSpec.describe Post do
-  it "user_idがあれば有効であること" do
+  it "user_id、video_idがあれば有効であること" do
     post = build(:post)
     expect(post).to be_valid
   end
@@ -27,5 +30,11 @@ RSpec.describe Post do
     post = build(:post, user_id: nil)
     post.valid?
     expect(post.errors[:user]).to include("を入力してください")
+  end
+
+  it "video_idがなければ無効であること" do
+    post = build(:post, video_id: nil)
+    post.valid?
+    expect(post.errors[:video]).to include("を入力してください")
   end
 end
