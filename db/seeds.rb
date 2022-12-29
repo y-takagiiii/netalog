2965aecb2135dc06e7d186b263f5b2013a748eff
yuritakagi <takagiyuri0712@gmail.com>
2022-12-29 09:30:12 +0900

    add: videoのテストデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,17 @@ User.create!(
   )
 end
 
+Video.create!(
+  youtube_id: '7lCDEYXw3mM',
+  title: 'Google I/O 101: Q&A On Using Google APIs',
+  description: 'Antonio Fuentes speaks to us and takes questions on working with Google APIs and OAuth 2.0.',
+  thumbnail: 'https://i.ytimg.com/vi/7lCDEYXw3mM/hqdefault.jpg',
+  view_count: 0
+)
+
 50.times do
   Post.create!(
-    user: User.offset(rand(User.count)).first
+    user: User.offset(rand(User.count)).first,
+    video: Video.offset(rand(Video.count)).first
   )
 end
