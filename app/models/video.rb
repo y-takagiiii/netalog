a719448d994a055bb690439c44b175dd68f5dea2
yuritakagi <takagiyuri0712@gmail.com>
2022-12-29 09:31:03 +0900

    add: バリデーションと関連付けを追加

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+#
+# Table name: videos
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  thumbnail   :string
+#  title       :string
+#  view_count  :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  youtube_id  :string
+#
 class Video < ApplicationRecord
+  has_many :posts
+
+  with_options presence: true do
+    validates :youtube_id
+    validates :title
+    validates :thumbnail
+    validates :view_count
+  end
 end
