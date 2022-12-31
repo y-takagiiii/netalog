b6b10f6a08cd5b7bbb416f31df5d34484bc8dbb2
yuritakagi <takagiyuri0712@gmail.com>
2022-12-31 11:46:25 +0900

    add: youtube data apiの確認 apiが正しく叩けるか確認 投稿にyoutube data apiから取り出した情報を紐付けられるか確認

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,20 @@
 class PostsController < ApplicationController
   def index
+
+    require 'google/apis/youtube_v3'
+
+    youtube = Google::Apis::YoutubeV3::YouTubeService.new
+    youtube.key = Rails.application.credentials.google[:api_key]
+
+    options = {
+      channel_id: 'UCwjx6ZG4pwCvAPSozYEWymA',
+      type: 'video',
+      max_results: 5
+    }
+
+    # **引数
+    @video_result = youtube.list_searches(:snippet, **options)
+
     @posts = Post.includes(:user)
   end
 end
