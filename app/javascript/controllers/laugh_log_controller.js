1a6b8418578ee3b3b23797a1e3e39ccd9d57a847
yuritakagi <takagiyuri0712@gmail.com>
2023-01-12 19:05:30 +0900

    create: laugh_logが作られることを確認するためのコードを作成

--- app/javascript/controllers/laugh_log_controller.js
+++ app/javascript/controllers/laugh_log_controller.js
@@ -2,6 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="laugh-log"
 export default class extends Controller {
-  connect() {
+  static targets = ["get"]
+
+  click() {
+    console.log('テストボタン動いてる？')
+  }
+  element_get() {
+    const element = this.getTarget
+    // const element = document.getElementById('player').getTarget
+    console.log(element)
   }
 }
