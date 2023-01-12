4a236ed99af7c2f7d3b81d2aedaff5bdb41dad3e
yuritakagi <takagiyuri0712@gmail.com>
2023-01-12 19:03:27 +0900

    add: videoのテストデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,27 @@ User.create!(
 end
 
 Video.create!(
-  youtube_id: '7lCDEYXw3mM',
-  title: 'Google I/O 101: Q&A On Using Google APIs',
-  description: 'Antonio Fuentes speaks to us and takes questions on working with Google APIs and OAuth 2.0.',
-  thumbnail: 'https://i.ytimg.com/vi/7lCDEYXw3mM/hqdefault.jpg',
-  view_count: 0
+  youtube_id: 'm9WzUSyOZYE',
+  title: 'ヨネダ2000【決勝ネタ】1st Round〈ネタ順8〉M-1グランプリ2022',
+  description: '【ヨネダ2000】 https://www.m-1gp.com/combi/12971.html ◇他の決勝ネタはこちら ...',
+  thumbnail: 'https://i.ytimg.com/vi/m9WzUSyOZYE/hqdefault.jpg',
+  view_count: 4_290_000
+)
+
+Video.create!(
+  youtube_id: 'LkmtzH5Z91o',
+  title: 'さや香【決勝ネタ】1st Round〈ネタ順5〉M-1グランプリ2022',
+  description: '【さや香】 https://www.m-1gp.com/combi/1125.html ◇他の決勝ネタはこちら ...',
+  thumbnail: 'https://i.ytimg.com/vi/LkmtzH5Z91o/hqdefault.jpg',
+  view_count: 73_100_000
+)
+
+Video.create!(
+  youtube_id: 'anC1McMAycw',
+  title: 'ウエストランド【決勝ネタ】最終決戦〈ネタ順1〉M-1グランプリ2022',
+  description: '【ウエストランド】 https://www.m-1gp.com/combi/1984.html ◇他の決勝ネタはこちら ...',
+  thumbnail: 'https://i.ytimg.com/vi/anC1McMAycw/hqdefault.jpg',
+  view_count: 5_250_000
 )
 
 50.times do
