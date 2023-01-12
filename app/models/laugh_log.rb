af6149a1f863d971a0d17dba4f179560b23c19ab
yuritakagi <takagiyuri0712@gmail.com>
2023-01-12 19:02:45 +0900

    create: laugh_logモデルを作成

--- app/models/laugh_log.rb
+++ app/models/laugh_log.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: laugh_logs
+#
+#  id                  :integer          not null, primary key
+#  button_pressed_time :integer
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  post_id             :integer          not null
+#
+# Indexes
+#
+#  index_laugh_logs_on_post_id  (post_id)
+#
+# Foreign Keys
+#
+#  post_id  (post_id => posts.id)
+#
 class LaughLog < ApplicationRecord
   belongs_to :post
+
+  validates :button_pressed_time, presence: true
 end
