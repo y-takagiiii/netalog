2713189e026cb8eb657a86575626f6a511487e46
yuritakagi <takagiyuri0712@gmail.com>
2023-02-04 08:26:32 +0900

    add: 検索結果をDBに保存するメソッドを作成

---
+++ app/models/youtube_api/save_video.rb
@@ -0,0 +1,16 @@
+module YoutubeApi::SaveVideo
+  require 'google/apis/youtube_v3'
+
+# 検索結果を保存するメソッド
+  def save_video(search_results)
+    search_results.each do |result|
+      Video.create!(
+        youtube_id: result[:video_id],
+        title: result[:title],
+        description: result[:description],
+        thumbnail: result[:thumbnail],
+        view_count: result[:view_count]
+      )
+    end
+  end
+end
