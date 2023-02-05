10cd14a5ba91628b8274a66d634593067cd10d79
yuritakagi <takagiyuri0712@gmail.com>
2023-02-06 08:04:30 +0900

    add: button_pressed_timeのデータ型を変更

--- app/models/laugh_log.rb
+++ app/models/laugh_log.rb
@@ -3,7 +3,7 @@
 # Table name: laugh_logs
 #
 #  id                  :integer          not null, primary key
-#  button_pressed_time :integer
+#  button_pressed_time :float
 #  created_at          :datetime         not null
 #  updated_at          :datetime         not null
 #  post_id             :integer          not null
@@ -20,4 +20,14 @@ class LaughLog < ApplicationRecord
   belongs_to :post
 
   validates :button_pressed_time, presence: true
+
+  # button_pressed_timeが整数もしくは浮動小数点数であることのバリデーションを追加
+  # validate :button_pressed_time_is_must_be_integer_or_float
+
+  # def button_pressed_time_is_must_be_integer_or_float
+  #   unless button_pressed_time.is_a?(Integer) || button_pressed_time.is_a?(Float)
+  #     errors.add(:button_pressed_time, "は無効な値です")
+  #   end
+  # end
+  # saveメソッドの後に実行されるから、クライアント側かコントローラレベルのバリデーションが必要？
 end
