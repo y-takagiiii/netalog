3fb65369f999a75476c040852e3d28ea3c9d26e5
yuritakagi <takagiyuri0712@gmail.com>
2023-02-18 20:26:23 +0900

    add: googleログインを実装

---
+++ app/controllers/google_logins_controller.rb
@@ -0,0 +1,35 @@
+class GoogleLoginsController < ApplicationController
+  skip_before_action :require_login
+
+  def oauth
+    login_at(auth_params[:provider])
+  end
+
+  def callback
+    provider = auth_params[:provider]
+    if auth_params[:denied].present? || auth_params[:error] == 'ACCESS_DENIED'
+      redirect_to login_path, warning: 'ログインをキャンセルしました'
+      return
+    end
+
+    begin
+      binding.b
+      create_user_from(provider) unless (@user = login_form(provider))
+      redirect_to posts_path, success: "#{provider.titleize}アカウントでログインしました"
+    rescue StandardError
+      redirect_to login_path, danger: "#{provider.titleize}アカウントでのログインに失敗しました"
+    end
+  end
+
+  private
+
+  def auth_params
+    params.permit(:code, :provider, :denied, :error, :scope, :authuser, :prompt)
+  end
+
+  def create_user_from(provider)
+    @user = create_from(provider)
+    reset_session
+    auto_login(@user)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,17 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   post 'guest_login', to: 'user_sessions#guest_login'
+  post 'google_login/callback', to: 'google_logins#oauth'
+  get 'google_login/callback', to: 'google_logins#oauth'
+  get 'google_login/:provider', to: 'google_logins#oauth', as: :auth_at_provider
   delete 'logout', to: 'user_sessions#destroy'
+
   get 'search', to: 'searches#search'
   get 'search_result', to: 'searches#result'
+
   get 'laugh_logs/new/:id', to: 'laugh_logs#new', as: 'new_laugh_log'
   post 'laugh_logs', to: 'laugh_logs#create'
+
   resources :users, only: %i[new create destroy]
   resources :posts, only: %i[index show destroy] do
     resource :likes, only: %i[create destroy]
