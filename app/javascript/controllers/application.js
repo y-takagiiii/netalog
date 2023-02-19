b3d3aeb0ea771365211d201eba6b5bded6e35824
yuritakagi <takagiyuri0712@gmail.com>
2023-02-19 20:24:53 +0900

    add: font-awesomeの導入

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,4 +1,10 @@
 import { Application } from "@hotwired/stimulus"
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core"
+import '@fortawesome/fontawesome-free'
+library.add(fas, far, fab)
 
 const application = Application.start()
 
