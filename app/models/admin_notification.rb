093fa043427fae43e63d21bc96d8c8d689bdb28e
yuritakagi <takagiyuri0712@gmail.com>
2023-03-08 06:17:06 +0900

    add: マイグレーションを実行

--- app/models/admin_notification.rb
+++ app/models/admin_notification.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: admin_notifications
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_admin_notifications_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class AdminNotification < ApplicationRecord
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_210417) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_211545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admin_notifications", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_admin_notifications_on_user_id"
+  end
 
   create_table "laugh_logs", force: :cascade do |t|
     t.integer "post_id", null: false
@@ -63,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_210417) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "admin_notifications", "users"
   add_foreign_key "laugh_logs", "posts"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
