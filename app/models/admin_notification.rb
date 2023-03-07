d73bd52dd4bad80cc15d76e2af15fd5523db77a9
yuritakagi <takagiyuri0712@gmail.com>
2023-03-08 06:19:21 +0900

    add: バリデーションを追加

--- app/models/admin_notification.rb
+++ app/models/admin_notification.rb
@@ -19,4 +19,7 @@
 #
 class AdminNotification < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :content, presence: true, length: { maximum: 300 }
 end
