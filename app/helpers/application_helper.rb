e668cd8519fd69f5fa73e1176b5fb3f456bb0a68
yuritakagi <takagiyuri0712@gmail.com>
2023-03-08 13:57:46 +0900

    add: meta_tagsの設定を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'ネタログ',
+      title: 'タイトル',
+      reverse: true,
+      separator: '|',
+      description: 'ページの説明',
+      keywords: 'お笑い, ネタ, ログ, ネタログ, コント',
+      charset: 'utf-8',
+      canonical: request.original_url,
+      noindex: ! Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.jpg'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        # site: '@ツイッターのアカウント名',
+        image: image_url('ogp.jpg')
+      }
+    }
+  end
 end
