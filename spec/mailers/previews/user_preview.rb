e87021e8d6bce5a233a1c39636a3de2aeb9fe6d0
yuritakagi <takagiyuri0712@gmail.com>
2023-03-08 17:55:51 +0900

    add: プレビューを設定

--- spec/mailers/previews/user_preview.rb
+++ spec/mailers/previews/user_preview.rb
@@ -3,7 +3,7 @@ class UserPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user/reset_password_email
   def reset_password_email
-    UserMailer.reset_password_email
+    UserMailer.with(to: "test_user@example.com", name: "test_user").reset_password_email
   end
 
 end
