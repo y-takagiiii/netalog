9a3ff6e2203b3b48ae676925526e8f3f7b5ac968
yuritakagi <takagiyuri0712@gmail.com>
2023-03-09 21:00:59 +0900

    add: ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
   resources :posts, only: %i[index show destroy] do
     resource :likes, only: %i[create destroy]
   end
+  resources :likes, only: :index
   resources :notifications, only: %i[index show]
 end
