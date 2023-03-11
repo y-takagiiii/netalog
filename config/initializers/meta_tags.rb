f7378e9285e44b65ca8acda0048862f89e43acf7
yuritakagi <takagiyuri0712@gmail.com>
2023-03-12 03:46:54 +0900

    add: gemをインストール

--- config/initializers/meta_tags.rb
+++ config/initializers/meta_tags.rb
@@ -1,22 +1,40 @@
-MetaTags.config do |config|
-  # Default title methods to be checked for title
-  #
-  # config.title_methods = %w(title name)
+# frozen_string_literal: true
 
-  # Default decsription methods to be checked for description
-  #
-  # config.description_methods = %w(description desc)
+# Use this setup block to configure all options available in MetaTags.
+MetaTags.configure do |config|
+  # How many characters should the title meta tag have at most. Default is 70.
+  # Set to nil or 0 to remove limits.
+  # config.title_limit = 70
 
-  # Default image methods to be checked for image
-  #
-  # config.image_methods = %w(image picture avatar)
+  # When true, site title will be truncated instead of title. Default is false.
+  # config.truncate_site_title_first = false
 
-  # When setting a title, keep original title after :
-  #
-  # Example : If you set the default title to "My Shop" and, on the home page
-  #   set the title to "Home", you'll end up with a title of : "Home - My Shop"
-  #
-  # Defaults to true
-  #
-  # config.keep_default_title_present = false
+  # Maximum length of the page description. Default is 300.
+  # Set to nil or 0 to remove limits.
+  # config.description_limit = 300
+
+  # Maximum length of the keywords meta tag. Default is 255.
+  # config.keywords_limit = 255
+
+  # Default separator for keywords meta tag (used when an Array passed with
+  # the list of keywords). Default is ", ".
+  # config.keywords_separator = ', '
+
+  # When true, keywords will be converted to lowercase, otherwise they will
+  # appear on the page as is. Default is true.
+  # config.keywords_lowercase = true
+
+  # When true, the output will not include new line characters between meta tags.
+  # Default is false.
+  # config.minify_output = false
+
+  # When false, generated meta tags will be self-closing (<meta ... />) instead
+  # of open (`<meta ...>`). Default is true.
+  # config.open_meta_tags = true
+
+  # List of additional meta tags that should use "property" attribute instead
+  # of "name" attribute in <meta> tags.
+  # config.property_tags.push(
+  #   'x-hearthstone:deck',
+  # )
 end
