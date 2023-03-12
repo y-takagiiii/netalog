80a378cab142f7b1d407b39dfc9201511174b8d4
yuritakagi <takagiyuri0712@gmail.com>
2023-03-12 18:08:40 +0900

    add: ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root 'tops#index'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
