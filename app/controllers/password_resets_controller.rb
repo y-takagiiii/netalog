f61d242c1ea0b76546b7736e8e78158daac63b03
yuritakagi <takagiyuri0712@gmail.com>
2023-03-12 15:58:23 +0900

    fix: コントローラを修正

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,19 +11,19 @@ class PasswordResetsController < ApplicationController
 
   def edit
     @token = params[:id]
-    @user = User.load_from_reset_password_token(params[:id])
+    @user = User.load_from_reset_password_token(@token)
     return not_authenticate if @user.blank?
   end
 
   def update
     @token = params[:id]
-    @user = User.load_from_reset_password_token(params[:id])
+    @user = User.load_from_reset_password_token(@token)
     return not_authenticate if @user.blank?
     @user.password_confirmation = params[:user][:password_confirmation]
     if @user.change_password(params[:user][:password])
       redirect_to login_path, success: "パスワードを変更しました。"
     else
-      flash.now[:danger] = "パスワードが変更できませんでした。"
+      flash.now[:error] = "パスワードが変更できませんでした。"
       render :edit
     end
   end
